479c705a2eb908649c83c5df00c6f0a582fcdd73
Huzaifa_073 <huzaifaidrees803@gmail.com>
2024-07-16 02:40:23 +0500

    first Api Commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+import 'Models/postsModel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +15,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+
+
+  // jha p array ho or uska nam na ho postman k response me
+  List<PostsModel> postList = [];
+  Future<List<PostsModel>> getPostApi() async{
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if(response.statusCode == 200){
+      postList.clear();
+      for(Map i in data){
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    }else{
+      return postList;
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +40,39 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.cyan.withOpacity(.4),
         title: const Text('API Integration'),
       ),
-      body: const Column(
+      body: Column(
         children: [
-
+         Expanded(
+           child: FutureBuilder(
+               future: getPostApi(),
+               builder: (context, snapshot){
+                 if (!snapshot.hasData) {
+                   return  const Center(child: Text('Loading'));
+                 } else{
+                   return ListView.builder(
+                     itemCount: postList.length,
+                       itemBuilder: (context,index){
+                       return Card(
+                         child: Padding(
+                           padding: const EdgeInsets.all(8.0),
+                           child: Column(
+                             mainAxisAlignment: MainAxisAlignment.start,
+                             crossAxisAlignment: CrossAxisAlignment.start,
+                             children: [
+                               Text('Title' , style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),),
+                               Text(postList[index].title.toString()),
+                               Text('Description' , style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                               Text(postList[index].body.toString()),
+                             ],
+                           ),
+                         ),
+                       );
+                       }
+                   );
+                 }
+               }
+           ),
+         )
         ],
       ),
     );
